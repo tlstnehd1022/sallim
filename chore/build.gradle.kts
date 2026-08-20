fb56f5dffe149b4f7dfddac3845eafe351325405
tlstnehd1022 <15190869+tlstnehd1022@users.noreply.github.com>
2026-08-20 14:26:47 +0900

    build: chore 모듈에 Spring Data JPA/MySQL/Flyway/Testcontainers 배선

--- chore/build.gradle.kts
+++ chore/build.gradle.kts
@@ -1,3 +1,24 @@
+import org.springframework.boot.gradle.plugin.SpringBootPlugin
+
+plugins {
+    alias(libs.plugins.kotlin.jpa)
+    alias(libs.plugins.spring.dependency.management)
+}
+
+dependencyManagement {
+    imports {
+        mavenBom(SpringBootPlugin.BOM_COORDINATES)
+    }
+}
+
 dependencies {
     implementation(project(":common"))
+    implementation(libs.spring.boot.starter.data.jpa)
+    implementation(libs.flyway.core)
+    runtimeOnly(libs.flyway.mysql)
+    runtimeOnly(libs.mysql.connector.j)
+
+    testImplementation(libs.spring.boot.starter.test)
+    testImplementation(libs.testcontainers.junit.jupiter)
+    testImplementation(libs.testcontainers.mysql)
 }
